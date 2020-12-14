656d8f7e5f3cd37d561e7898935b5759e6363f02
leejaek <67036362+leejaek@users.noreply.github.com>
2020-12-15 01:50:18 +0900

    settings.py, middleware 오류 수정

--- youngchapedia/settings.py
+++ youngchapedia/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
 #    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'youngchapedia.urls'

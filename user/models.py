a0041880a8c03f239b2ab6a12afb99f5596afa31
leejaek <67036362+leejaek@users.noreply.github.com>
2020-12-16 12:12:06 +0900

    models.py 생성, App 구분없이 User  App에 통합 생성

---
+++ user/models.py
@@ -0,0 +1,313 @@
+from django.db              import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils           import timezone
+
+from content.models import Content
+
+
+class User(models.Model):
+    email            = models.CharField(max_length=45)
+    password         = modles.CharField(max_length=250)
+    profile_image    = models.CharField(max_length=1000, null=True)
+    user_bio         = models.TextField(max_length=300, null=True)
+    language         = models.ForeignKey(Language, on_delete=models.SET_DEFAULT, default=1)
+    country          = models.ForeignKey(Country, on_delete=models.SET_DEFAULT, default=1)
+    disclosure_scope = models.ForeignKey(Disclosure, on_delete=models.SET_DEFAULT, default=1)
+    background_image = models.CharField(max_length=1000, null=True)
+    username         = models.CharField(max_length=20)
+    relations        = models.ManyToManyField('self', through='Relation', symmetrical=False)
+    class Meta:
+        db_table = 'users'
+
+
+class Relation(models.Model):
+    from_user       = models.ForeignKey(User, on_delete = models.CASCADE, related_name='relations_by_from_user')
+    to_user         = models.ForeignKey(User, on_delete = models.CASCADE, related_name='relations_by_to_user')
+    relation_status = models.ForeignKey(RelationStatus, on_delete = models.CASCADE)
+    class Meta:
+        db_table = 'relations'
+
+
+class RelationStatus(models.Model):
+    status = models.CharField(max_length=20)
+    class Meta:
+        db_table = 'relation_status'
+
+
+class Partner(models.Model):
+    request_user   = models.ForeignKey(User, on_delete = models.CASCADE, related_name='requested_user_id')
+    requested_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name='request_user_id')
+    request_status = models.BooleanField()
+    class Meta:
+        db_table = 'partners'
+
+
+class Rating(models.Model):
+    user       = models.ForeignKey(User, on_delete = models.CASCADE, related_name='rated_movie')
+    content    = models.ForeignKey(Content, on_delete = models.CASCADE, related_name='rating_user')
+    rating     = models.FloatField(validators=[MinValueValidator(0.5), MaxValueValidator(5.0)])
+    watch_date = models.DateField(null=True) 
+    create_time = models.DateTimeField(default = timezone.now)
+    class Meta:
+        db_table = 'ratings'
+
+
+class Archive(models.Model):
+    user         = models.ForeignKey(User, on_delete=models.CASCADE, related_name='archives')
+    content      = models.ForeignKey(Content, on_delete=models.CASCADE)
+    archive_type = models.ForeignKey(ArchiveType, on_delete=models.CASCADE)
+    created_at   = models.DateTimeField(timezone.now)
+    updated_at   = models.DateTimeField()
+    class Meta:
+        db_table = 'archives'
+
+
+class ArchiveType(models.Model):
+    name = models.ForeignKey(ArchiveType, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'archivetypes'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'languages'
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'countries'
+
+
+class Disclosure(models.Model):
+    name  = models.CharField(max_length=10)
+    class Meta:
+        db_table = 'dislosures'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'categories'
+
+
+class Content(models.Model):
+    title_korean = models.CharField(max_length=100)
+    main_photo   = models.CharField(max_length=1000)
+    category     = models.ForeignKey(Category, on_delete=models.CASCADE)
+    release_year = models.IntegerField(default=0000, null=True)
+    class Meta:
+        db_table = 'contents'
+
+
+class MovieOverview(models.Model):
+    title_original = models.CharField(max_length=100)
+    runtime        = models.IntegerField(default=0, null=True)
+    description    = models.TextField(max_length=200, null=True)
+    content        = models.ForeignKey(Content, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'movie_overviews'
+
+
+class TvProgramOverview(models.Model):
+    title_original = models.CharField(max_length=100)
+    broadcast      = models.CharField(max_length=45, null=True)
+    description    = models.TextField(max_lenth=200, null=True)
+    content        = models.ForeignKey(Content, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'tv_program_overviews'
+
+
+class BookOverview(models.Model):
+    sub_title              = models.CharField(max_length=45)
+    sub_category           = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    page                   = models.IntegerField(default=0, null=True)
+    book_contents          = models.TextField(max_length=1000, null=True)
+    publisher_introduction = models.TextField(max_length=1000, null=True)
+    content                = models.ForeignKey(Content, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'book_overviews'
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=20)
+    class Meta:
+        db_table = 'subcategories'
+
+
+class ContentPhoto(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    photo   = models.CharField(max_length=1000)
+    class Meta:
+        db_table = 'content_photos'
+
+
+class Staff(models.Model):
+    name      = models.ForeignKey(People, on_delete=models.CASCADE)
+    content   = models.ForeignKey(Content, on_delete=models.CASCADE)
+    job       = models.ForeignKey(job, on_delete=models.SET_NULL)
+    role_name = models.CharField(null=True)
+    class Meta:
+        db_table = 'staffs'
+
+
+class People(models.Model):
+    name        = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    class Meta:
+        db_table = 'people'
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'jobs'
+
+
+class PeopleJob(models.Model):
+    people = models.ForeignKey(People, on_delete=models.CASCADE)
+    job    = models.ForeignKey(Job, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'people_jobs'
+
+
+class StaffJobs(models.Model):
+    staff   = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    job     = models.ForeignKey(Job, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'staff_jobs'
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'genres'
+
+
+class ContentGenre(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    genre   = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'content_genres'
+
+
+class ContentCountry(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'content_countries'
+
+
+class ContentService(models.Model):
+    name = models.CharField(max_legnth=20)
+    class Meta:
+        db_table = 'content_services'
+
+
+class ContentAvailableService(models.Model):
+    content         = models.ForeignKey(Content, on_delete=models.CASCADE)
+    content_service = models.ForeignKey(ContentService, on_delete=models.CASCADE)
+    price           = models.IntegerField(null=True)
+    link            = models.URLField(max_length=1000)
+    class Meta:
+        db_table = 'contents_available_services'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    class Meta:
+        db_table = 'tags'
+
+
+class ContentTag(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)
+    tag     = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'content_tags'
+
+
+class Review(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    content     = models.ForeignKey(Content, on_delete=models.CASCADE)
+    body        = models.TextField(max_length=1000)
+    created_at  = models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = 'reviews'
+
+
+class ReviewLike(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    review     = models.ForeignKey(Review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = 'review_likes'
+
+
+class ReviewReport(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    review     = models.ForeignKey(review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = 'review_reports'
+
+
+class ReviewComment(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    review     = models.ForeignKey(Review, on_delete=models.CASCADE)
+    body       = models.CharField(max_legnth=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = 'review_comments'
+
+
+class ReviewCommentLike(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment    = models.ForeignKey(ReviewComments, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = 'review_comment_likes'
+
+
+class ContentCollection(models.Model):
+    content    = models.ForeignKey(Content, on_delete=models.CASCASE)
+    collection = models.ForeignKey(Content, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'content_collections'
+
+
+class Collection(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    title      = models.CharField(max_length=40)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField()
+    class Meta:
+        db_table = 'collections'
+
+
+class CollectionLike(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        db_table = 'collection_likes'
+
+
+class CollectionComment(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    body       = models.CharField(max_legnth=100)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField()
+    class Meta:
+        db_table = 'collection_comments'
+
+
+class CollectionCommentLike(models.Model):
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment    = models.ForeignKey(ReviewComments, on_delete=models.CASCADE)
+    created_at = models.TimeField(default=timezone.now)
+    class Meta:
+        db_table = 'collection_comment_likes'
+
+
---
+++ user/views.py
@@ -0,0 +1,55 @@
+import re
+import json
+import bcrypt
+
+from django.http  import JsonResponse
+from django.views import View
+
+from user.models  import User
+from my_settings  import SECRET_KEY, ALGORITHM
+
+REGEX_EMAIL        = '([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+(\.[a-zA-Z]{2,4}))'
+REGEX_PASSWORD     = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'
+
+class SignUp(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            name     = data['name']
+            email    = data['email']
+            password = data['password']
+            language = 'ko-kr'
+            country  = 'South Korea'
+            
+            
+            assert not User.objects.filter(name = name), "ALREADY_EXISTS_ACCOUNT"
+            assert re.match(REGEX_EMAIL, email), "INVALID_EMAIL_FORMAT"
+            assert re.match(REGEX_PASSWORD, password), "INVALID_PASSWORD_FORMAT"
+
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+
+            User.objects.create(email = email, password = hashed_pasword, language = language, country = country, username = name)
+            return JsonResponse({"message": "SUCCESS"}, status = 201)
+        except json.JSONDecodeError as e:
+            return JsonResponse({"message": f"{e}"}, status = 400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+        except AssertionError as e:
+            return JsonResponse({"message": f"{e}"}, status = 400)
+
+class SignIn(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            email    = data['email']
+            password = data['password']
+
+            signin_user = User.objects.get(email = email)
+
+            if bcrypt.checkpw(password.encode('utf-8'), signin_user.password.encode('utf-8')):
+                access_token = jwt.encode({'user_id': signin_user.id}, SECRET_KEY, algorithm = ALGORITHM)
+                return JsonResponse({"message": "SUCCESS", "access_token": access_token.decode('utf-8')}, status = 200)
+            else:
+                return JsonResponse({"message": "INVALID_USER_NAME_OR_PASSWORD"}, status = 401)

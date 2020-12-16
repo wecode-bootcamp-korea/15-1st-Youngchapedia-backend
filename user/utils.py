392f481fe9125fe1cbbcdf6649f8efbce5051576
leejaek <67036362+leejaek@users.noreply.github.com>
2020-12-16 21:53:14 +0900

    signup/signin 기능 추가

---
+++ user/utils.py
@@ -0,0 +1,25 @@
+import jwt
+import json
+from functools import wraps
+
+from django.http import JsonResponse
+
+from user.models import User
+from my_settings import SECRET_KEY, ALGORITHM
+
+def id_auth(func):
+    @wraps(func)
+    def decorated_function(self, request, *args, *kwargs):
+        try:
+            access_token = request.headers.get('access_token')
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+            login_user   = User.objects.get(id = payload['user_id'])
+            request.user = login_user
+            
+            return func(self, request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({"message": "INVALID_TOKEN"}, status = 401)
+        except User.DoesNotExist:
+            return JsonResponse({"message": "INVALID_USER"}, status = 401)
+    return decorated_function

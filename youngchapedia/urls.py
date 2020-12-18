b8fe9f5d50d0dc5b3e25bdbbf1b404585fa32f7b
kst6294 <kst6294@gmail.com>
2020-12-18 11:20:01 +0900

    #1-1 conflict modified

--- youngchapedia/urls.py
+++ youngchapedia/urls.py
@@ -17,6 +17,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('user', include('user.urls')),
     path('contents', include('content.urls')),
+    path('user', include('user.urls')),
 ]

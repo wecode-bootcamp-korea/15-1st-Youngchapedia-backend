4baf94c7409c90c0c60b3a82736806eaf9d85837
kst6294 <kst6294@gmail.com>
2020-12-18 16:11:26 +0900

    Modified: content/view 수정 - PeopleContent : 동작하지 않는 if 조건문 코드 삭제
    Add: content/view api 추가
    - GenreContent / TagContent 추가
    - 클라이언트에서 Genre_id/Tag_id 를 보내주면 genre_id/tag_id로 content filter 하고
      genre_id/tag_id 와 연결된 contents API 생성 및 응답
    
    remove: .DS_Store 삭제
    - .DS_Store 추적하지 않도록 .gitignore 에 .DS_Store 를 추가

--- content/urls.py
+++ content/urls.py
@@ -5,4 +5,6 @@ from content.views import *
 urlpatterns = [
 #    path('', MainContent.as_view()),
     path('/people/<int:people_id>', PeopleContent.as_view()),
+    path('/genre/<int:genre_id>', GenreContent.as_view()),
+    path('/tag/<int:tag_id>', TagContent.as_view()),
 ]
--- content/views.py
+++ content/views.py
@@ -6,67 +6,138 @@ from django.views import View
 from content.models import *
 
 # Create your views here.
-"""
-작업 중단한 코드
-class MainContent(View):
-    def get(self, request):
-        contents = Content.objects.all()
-        results  = []
-
-        for content in contents:
-            genres    = ContentGenre.objects.filter(content_id = content.id)
-            tags      = ContentTag.objects.filter(content_id = content.id)
-            countries = ContentCountry.objects.filter(content_id = content.id)
-            results.append(
-                {
-                    'title_korean'   : content.title_korean,
-                    'title_original' : content.movieoverview_set.get().title_original,
-                    'id'             : content.id,
-                    'main_image_url' : content.main_image_url,
-                    'release_year'   : content.release_year,
-                    'runtime'        : content.movieoverview_set.get().runtime,
-                    'description'    : content.movieoverview_set.get().description,
-                    'genre'          : [genre.genre.name for genre in genres],
-                    'tag'            : [tag.tag.name for tag in tags],
-                }
-            )
-        return JsonResponse({'RESULT': results}, status=200)
-"""
 
 class PeopleContent(View):
     def get(self, request, people_id):
         try:
-
             people       = People.objects.get(id=people_id)
             jobs         = people.contentpeople_set.all()
-            # job_list 중복 제거
             job_list     = list(set([job.job.name for job in jobs]))
             contents     = ContentPeople.objects.filter(people_id=people_id)
             content_list = []
 
             for content in contents:
-                if content.content.id not in content_list:
-                    content_list.append(
-                        {
-                            'id'             : content.content.id,
-                            'title_korean'   : content.content.title_korean,
-                            'title_original' : content.content.movieoverview_set.get().title_original,
-                            'category'       : content.content.category.name,
-                            'main_image_url' : content.content.main_image_url,
-                       }
+                content_list.append(
+                    {
+                        'id'             : content.content.id,
+                        'title_korean'   : content.content.title_korean,
+                        'title_original' : content.content.movieoverview_set.get().title_original,
+                        'category'       : content.content.category.name,
+                        'main_image_url' : content.content.main_image_url,
+                    }
                 )
-
-            # content_list 중복 제거
             content_list = [dict(new_content) for new_content in {tuple(content.items()) for content in content_list}]
             results      = [
                 {
                     'name'              : people.name,
                     'title'             : job_list,
                     'profile_image_url' : people.profile_image_url,
-                    'content'           : content_list,
+                    'contents'          : content_list,
                 }
             ]
             return JsonResponse({'RESULT' : results}, status=200)
 
         except People.DoesNotExist:
             return JsonResponse({'ERROR' : 'PEOPLE_ERROR'}, status=400)
+
+        except Exception as e:
+            return JsonResponse({'ERROR' : e}, status=400)
+
+
+class GenreContent(View):
+    def get(self, request, genre_id):
+        try:
+            genre        = Genre.objects.get(id=genre_id)
+            contents     = ContentGenre.objects.filter(genre_id=genre_id)
+            content_list = []
+
+            for content in contents:
+                content_list.append(
+                    {
+                        'id'             : content.content.id,
+                        'title_korean'   : content.content.title_korean,
+                        'title_original' : content.content.movieoverview_set.get().title_original,
+                        'category'       : content.content.category.name,
+                        'main_image_url' : content.content.main_image_url,
+                    }
+                )
+            content_list = [dict(new_content) for new_content in {tuple(content.items()) for content in content_list}]
+            results      = [
+                {
+                    'genre_id'   : genre.id,
+                    'genre_name' : genre.name,
+                    'contents'   : content_list,
+                }
+            ]
+            return JsonResponse({'MESSAGE' : results}, status=200)
+
+        except Genre.DoesNotExist:
+            return JsonResponse({'ERROR' : 'GENRE_ERROR'}, status=400)
+
+        except Exception as e:
+            return JsonResponse({'ERROR' : e}, stauts=400)
+
+
+class TagContent(View):
+    def get(self, request, tag_id):
+        try:
+            tag          = Tag.objects.get(id=tag_id)
+            contents     = ContentTag.objects.filter(tag_id=tag_id)
+            content_list = []
+
+            for content in contents:
+                content_list.append(
+                    {
+                        'id'             : content.content.id,
+                        'title_korean'   : content.content.title_korean,
+                        'title_original' : content.content.movieoverview_set.get().title_original,
+                        'category'       : content.content.category.name,
+                        'main_image_url' : content.content.main_image_url,
+                    }
+                )
+            content_list = [dict(new_content) for new_content in {tuple(content.items()) for content in content_list}]
+            results      = [
+                {
+                    'tag_id'   : tag.id,
+                    'tag_name' : tag.name,
+                    'contents' : content_list,
+                }
+            ]
+            return JsonResponse({'RESULT' : results}, status=200)
+
+        except Tag.DoesNotExist:
+            return JsonResponse({'ERROR' : 'TAG_ERROR'}, status=400)
+
+        except Exception as e:
+            return JsonResponse({'ERROR' : e}, status=400)
+
+
+
+
+
+# 작업 중단한 코드
+#class MainContent(View):
+#    def get(self, request):
+#        contents = Content.objects.all()
+#        results  = []
+#
+#        for content in contents:
+#            genres    = ContentGenre.objects.filter(content_id = content.id)
+#            tags      = ContentTag.objects.filter(content_id = content.id)
+#            countries = ContentCountry.objects.filter(content_id = content.id)
+#            results.append(
+#                {
+#                    'title_korean'   : content.title_korean,
+#                    'title_original' : content.movieoverview_set.get().title_original,
+#                    'id'             : content.id,
+#                    'main_image_url' : content.main_image_url,
+#                    'release_year'   : content.release_year,
+#                    'runtime'        : content.movieoverview_set.get().runtime,
+#                    'description'    : content.movieoverview_set.get().description,
+#                    'genre'          : [genre.genre.name for genre in genres],
+#                    'tag'            : [tag.tag.name for tag in tags],
+#                }
+#            )
+#        return JsonResponse({'RESULT': results}, status=200)
+
+

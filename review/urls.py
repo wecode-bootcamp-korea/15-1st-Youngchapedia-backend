63cdbdfce0b5033a6cba7ea62a30de8632c5421c
leejaek <67036362+leejaek@users.noreply.github.com>
2020-12-21 09:41:45 +0900

    Add: 영화별 유저 review 리스트 조회 기능

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from review.views import ReviewView
+from review.views import ReviewView, ContentReviewView
 
 urlpatterns = [
     path('/content/<int:content_pk>', ReviewView.as_view()),
+    path('/list/content/<int:content_pk>', ContentReviewView.as_view()),
 ]

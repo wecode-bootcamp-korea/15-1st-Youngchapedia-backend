92b413a6e5ad3c82709cc66d7f21b6742e0dfefe
leejaek <67036362+leejaek@users.noreply.github.com>
2020-12-23 21:23:03 +0900

    Fix: ORM 개선을 통한 검색 기능 프로세스 개선 - prefetch_related, select_related를 활용한 검색기능 개선

--- content/views.py
+++ content/views.py
@@ -13,17 +13,18 @@ class PeopleContent(View):
             people       = People.objects.get(id=people_id)
             jobs         = people.contentpeople_set.all()
             job_list     = list(set([job.job.name for job in jobs]))
-            contents     = ContentPeople.objects.filter(people_id=people_id)
+            contents     = Content.objects.filter(id__in = ContentPeople.objects.filter(people = people)).select_related('category').prefetch_related('movieoverview_set') 
+            #contents     = ContentPeople.objects.filter(people_id=people_id).select_related('content', 'people')
             content_list = []
 
             for content in contents:
                 content_list.append(
                     {
-                        'id'             : content.content.id,
-                        'title_korean'   : content.content.title_korean,
-                        'title_original' : content.content.movieoverview_set.get().title_original,
-                        'category'       : content.content.category.name,
-                        'main_image_url' : content.content.main_image_url,
+                        'id'             : content.id,
+                        'title_korean'   : content.title_korean,
+                        'title_original' : content.movieoverview_set.all()[0].title_original,
+                        'category'       : content.category.name,
+                        'main_image_url' : content.main_image_url,
                     }
                 )
             results      = [
@@ -44,17 +45,18 @@ class GenreContent(View):
     def get(self, request, genre_id):
         try:
             genre        = Genre.objects.get(id=genre_id)
-            contents     = ContentGenre.objects.filter(genre_id=genre_id)
+            contents     = Content.objects.filter(id__in = ContentGenre.objects.filter(genre = genre)).select_related('category').prefetch_related('movieoverview_set')
+            #contents     = ContentGenre.objects.filter(genre_id=genre_id)
             content_list = []
 
             for content in contents:
                 content_list.append(
                     {
-                        'id'             : content.content.id,
-                        'title_korean'   : content.content.title_korean,
-                        'title_original' : content.content.movieoverview_set.get().title_original,
-                        'category'       : content.content.category.name,
-                        'main_image_url' : content.content.main_image_url,
+                        'id'             : content.id,
+                        'title_korean'   : content.title_korean,
+                        'title_original' : content.movieoverview_set.all()[0].title_original,
+                        'category'       : content.category.name,
+                        'main_image_url' : content.main_image_url,
                     }
                 )
             results      = [
@@ -72,17 +74,18 @@ class TagContent(View):
     def get(self, request, tag_id):
         try:
             tag          = Tag.objects.get(id=tag_id)
-            contents     = ContentTag.objects.filter(tag_id=tag_id)
+            
+            contents     = Content.objects.filter(id__in = ContentTag.objects.filter(tag = tag)).select_related('category').prefetch_related('movieoverview_set')
             content_list = []
 
             for content in contents:
                 content_list.append(
                     {
-                        'id'             : content.content.id,
-                        'title_korean'   : content.content.title_korean,
-                        'title_original' : content.content.movieoverview_set.get().title_original,
-                        'category'       : content.content.category.name,
-                        'main_image_url' : content.content.main_image_url,
+                        'id'             : content.id,
+                        'title_korean'   : content.title_korean,
+                        'title_original' : content.movieoverview_set.all()[0].title_original,
+                        'category'       : content.category.name,
+                        'main_image_url' : content.main_image_url,
                     }
                 )
             results      = [
